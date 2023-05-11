c126e62b9389040924d949c28e415fe34e58e99e
Maksym Davydenko <maksydenko@gmail.com>
2023-05-11 21:54:37 +0300

    Minor fixes

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,9 +3,16 @@ import Link from "next/link";
 
 import ThemeSwitcher from "./ThemeSwitcher/ThemeSwitcher";
 
+import { Bebas_Neue } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 const Header: FC = () => (
   <header className="header">
-    <div className="header__container">
+    <div className={`header__container ${bebasNeue.className}`}>
       <Link href="/" className="header__logo">
         To do
       </Link>

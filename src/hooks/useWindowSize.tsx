9a3853846431dc8cd2a87fb6e6de8c4ffed1a4d6
Maksym Davydenko <maksydenko@gmail.com>
2023-06-01 17:03:34 +0300

    Minor fixes

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 
 interface IUseWindowSize {
-  (handler: () => void, dependency: boolean | string): void;
+  (handler: () => void): void;
 }
 
-export const useWindowSize: IUseWindowSize = (handler, dependency) => {
+export const useWindowSize: IUseWindowSize = (handler) => {
   useEffect(() => {
     window.addEventListener("resize", handler);
 
     return () => {
       window.removeEventListener("resize", handler);
     };
-  }, [dependency]);
+  }, []);
 };
